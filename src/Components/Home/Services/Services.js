d6915d0de3a33c8d92d534568a2c06ef84ab8204
Sumonche016 <sumonche016@gmail.com>
2022-05-09 11:16:40 +0600

    manage btn added

--- src/Components/Home/Services/Services.js
+++ src/Components/Home/Services/Services.js
@@ -9,15 +9,20 @@ const Services = () => {
             .then(res => res.json())
             .then(data => setServices(data))
     }, [])
+
     return (
         <div className='py-5 service-area'>
             <div className="container">
                 <div className="row">
                     {
-                        services.map(service => <Service key={service._id} service={service}></Service>)
+                        services.slice(0, 6).map(service => <Service key={service._id} service={service}></Service>)
                     }
                 </div>
 
+
+            </div>
+            <div className='d-flex justify-content-center'>
+                <button className=' btn-manage'>Manage Inventories</button>
 
             </div>
         </div>
